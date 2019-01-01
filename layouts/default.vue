445c2a5764e9818e3d435db6ed990437274d6dcc
m-tanaka-ken <masaki.ken.yamada@googlemail.com>
2019-01-01 18:20:46 +0900

    reboot.css追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
+    <global-header/>
     <div class="container">
-      <global-header/>
       <nuxt/>
     </div>
     <global-sidebar/>
@@ -18,22 +18,4 @@ export default {
 </script>
 
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,7 @@ module.exports = {
   /*
   ** Global CSS
   */
-  css: [],
+  css: ['@/assets/css/reboot.css'],
 
   /*
   ** Plugins to load before mounting the App
