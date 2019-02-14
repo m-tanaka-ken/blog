d1c578ba2e20bb157b3505eb2587b5e20980f27e
m-tanaka-ken <masaki.ken.yamada@googlemail.com>
2019-02-15 01:25:40 +0900

    webstorm用にコンフィグ変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -78,5 +78,14 @@ module.exports = {
         });
       }
     }
+  },
+
+  resolve: {
+    extensions: ['.js', '.json', '.vue', '.ts'],
+    root: path.resolve(__dirname),
+    alias: {
+      '@': path.resolve(__dirname),
+      '~': path.resolve(__dirname),
+    },
   }
 };
