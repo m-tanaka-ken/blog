f55ca4840cf41184652ac367c96797853b0d09da
m-tanaka-ken <masaki.ken.yamada@googlemail.com>
2019-02-16 20:17:13 +0900

    package update

--- nuxt.config.js
+++ nuxt.config.js
@@ -85,7 +85,7 @@ module.exports = {
     root: path.resolve(__dirname),
     alias: {
       '@': path.resolve(__dirname),
-      '~': path.resolve(__dirname),
-    },
+      '~': path.resolve(__dirname)
+    }
   }
 };
